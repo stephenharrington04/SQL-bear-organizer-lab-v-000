12a29fa501ed3971a81b0968b2522081f076c1c3
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-09 15:45:39 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Mean", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "M", "Brown", "Mean", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "M", "Brown", "Mean", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "Brown", "Mean", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "M", "Brown", "Mean", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "M", "Brown", "Mean", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "M", "Brown", "Mean", "1");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "M", "Brown", "Mean", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "Sleepy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "Black", "Happy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "White", "Mean", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "M", "Brown", "Happy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "F", "Black", "Sleepy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "Blond", "Happy", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Black", "Mean", "1");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "M", "Brown", "Mean", "1");
